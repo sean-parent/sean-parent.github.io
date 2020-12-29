033520a43a495dadf6c21049f4b0a09eed5add1b
Felix Petriconi <felix.petriconi@mevis.de>
2020-12-29 17:32:46 +0100

    Let code and figures have different index
    Remove accidentally added files
    Give Amdahl's graphs dotted and dashed lines

--- better-code/figures/R/amdahl_log.R
+++ better-code/figures/R/amdahl_log.R
@@ -1,32 +1,43 @@
 library(ggplot2)
 library(scales)
 
-amdahlFun95 <- function(x) {
-    1.0 / ((1.0 - 0.95) + 0.95 / x)
-}
-amdahlFun90 <- function(x) {
-    1.0 / ((1.0 - 0.9) + 0.9 / x)
-}
-amdahlFun75 <- function(x) {
-    1.0 / ((1.0 - 0.75) + 0.75 / x)
-}
-amdahlFun50 <- function(x) {
-    1.0 / ((1.0 - 0.50) + 0.50 / x)
-}
+amdahl <- function(x, p) {1.0 / ((1.0 - p) + p / x)}
 
-p1 <- ggplot(data.frame(x = c(1, 65536)), aes(x=x)) + 
-    stat_function(fun = amdahlFun95, aes(colour = "95%")) +
-    stat_function(fun = amdahlFun90, aes(colour = "90%")) +
-    stat_function(fun = amdahlFun75, aes(colour = "75%")) +
-    stat_function(fun = amdahlFun50, aes(colour = "50%")) +
+amdahlFun05 <- function(x) amdahl(x, 0.95)
+amdahlFun10 <- function(x) amdahl(x, 0.90)
+amdahlFun25 <- function(x) amdahl(x, 0.75)
+amdahlFun50 <- function(x) amdahl(x, 0.50)
+
+LegendTitle = "Percentage of\nsynchronization"
+
+p1 <- ggplot(data = data.frame(x = c(1, 65536)), mapping = aes(x=x)) + 
+    stat_function(fun = amdahlFun05, mapping = aes(colour = "amdahlFun05", linetype = "amdahlFun05")) +
+    stat_function(fun = amdahlFun10, mapping = aes(colour = "amdahlFun10", linetype = "amdahlFun10")) +
+    stat_function(fun = amdahlFun25, mapping = aes(colour = "amdahlFun25", linetype = "amdahlFun25")) +
+    stat_function(fun = amdahlFun50, mapping = aes(colour = "amdahlFun50", linetype = "amdahlFun50")) +
     scale_x_continuous(trans = 'log2', name = "Number of Processors", limits=c(1, 65536), 
         breaks = trans_breaks("log2", n = 16, function(x) 2^x), labels = trans_format("log2", function(x) 2^x)) +
     scale_y_continuous(name = "Speedup", breaks = seq(0, 21, 2), limits=c(1, 20)) +
-        scale_colour_manual("Parallel Portion", 
-        limits=c("95%", "90%", "75%", "50%"), 
-        values = c("95%" = "red", "90%" = "orange", "75%" = "deeppink", "50%" = "blue")) +
-    theme_bw()
-    
- ggsave(file="05_amdahl_log.svg", plot=p1, width=12, height=10)
+    scale_colour_manual(LegendTitle, 
+        values = c("amdahlFun05" = "red", "amdahlFun10" = "orange", "amdahlFun25" = "deeppink", "amdahlFun50" = "blue"),
+        labels = c("5%", "10%", "25%", "50%")) +
+    scale_linetype_manual(LegendTitle, 
+        values = c("amdahlFun05" = "solid", "amdahlFun10" = "dashed", "amdahlFun25" = "longdash", "amdahlFun50" = "dotted"),
+        labels = c("5%", "10%", "25%", "50%")) + 
+    theme_bw() +
+    theme(axis.line = element_line(size=1, colour = "black"),
+              panel.grid.major = element_line(colour = "#d3d3d3"),
+              panel.grid.minor = element_blank(),
+              panel.border = element_blank(), panel.background = element_blank(),
+              text=element_text(family="Arial"),
+              legend.title=element_text(size = 10, family = "Arial"),
+              legend.text = element_text(size = 10),
+              legend.position = c(0.25, 0.8),
+              axis.text.x=element_text(colour="black", size = 8),
+              axis.text.y=element_text(colour="black", size = 8),
+              axis.title.x=element_text(colour="black", size = 12),
+              axis.title.y=element_text(colour="black", size = 12))
+              
+ ggsave(file="../05-amdahl_log.svg", plot=p1, width=8, height=6)
 
 
